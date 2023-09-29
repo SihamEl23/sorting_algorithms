db488c006e036e3336251fa41468a047ef0e3e1c
SihamEl23 <elghazi.siham1101@gmail.com>
2023-09-29 12:50:16 -0700

    task 3 done

--- sort.h
+++ sort.h
@@ -23,9 +23,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort_helper(int *array, int imin, int imax, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
 int partition(int *t, int imin, int imax, size_t size);
-void quick_sort_helper(int *array, int imin, int imax, size_t size);
 
 #endif
